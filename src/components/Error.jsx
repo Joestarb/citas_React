a87bb8052e17634557da2ee56afb2de6a4fe3197
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2023-08-09 14:36:53 -0500

    final job

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+
+function Error({children}) {
+  return (
+    <div className="   ml-4 rounded-lg bg-red-800 p-3   text-white text-center font-semibold">
+        {children}
+  </div>
+
+
+  )
+}
+
+export default Error
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.jsx"],//definir archivos de en donde estara css
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
