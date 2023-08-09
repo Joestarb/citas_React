334c8e310f90725d7d5a02de817742a576262533
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2023-08-09 14:55:10 -0500

    commit deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.jsx"],//definir archivos de en donde estara css
+  content: ["index.html", "./src/**/*.jsx"],//definir archivos de en donde estara css
   theme: {
     extend: {},
   },
